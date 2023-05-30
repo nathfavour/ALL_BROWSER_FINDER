89c248178eceb9c59da39656a02b218e04464bf1
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-05-29 18:31:43 -0700

    ALL_BROWSER_FINDER

---
+++ login.py
@@ -0,0 +1,36 @@
+import datetime
+import sys
+import time
+
+
+# ALL BROWSER SEARCH-ENGINE
+print("loading.")
+print("loading..")
+print("loading...")
+now = datetime.datetime.now()
+print("Browser@generalist_developer => ", end="")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+logos = """
+                                     .////\\\\\\\\        \\\''''''\\\\\\\\\         '.\\\\?///////////
+                                     .?////  ..\\\\      .... \\\      \\\     .??????''''''''
+                                    ...>>>>   ....>>>   .....||||       ?\\    ...\\\   
+                                   ...>>>>     ...>>>>   ....\|||      i\.     ...||........
+                                  ...||||       /..||||    ....|\\||???i.i    ....||////////
+                                 ...||||        ..;;||||    ...||||      \\    ....|||
+                               ....||||/\/\/\/\/\/\/\||||   ...///        \\    .././
+                             ....||||               ..|||| ....///        ||    eeb?//.
+                              ..||||                 ...|||..?///........//.....cc?///.
+                                        
+                                        All   Browsers     Finder
+
+                            """
+
+print(logos)
+
+
+print(now)
+
+
+print("Successful login!")
+time.sleep(1)
